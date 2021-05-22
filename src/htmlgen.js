50df6e384fb30c2cc1178c745f27cea88ea3e994
chdonovan <chrisdonovan311@gmail.com>
2021-05-22 16:22:35 -0500

    manger card html function

--- src/htmlgen.js
+++ src/htmlgen.js
@@ -1,6 +1,21 @@
 // manager card function
 const generateManager = function (manager) {
-    r
+    return `
+    <div class = "col-4 mt-4">
+        <div class = "card h-100">
+            <div class = "card-header>"
+                <h3> ${manager.name}</h3>
+                <h4> Manager</h4>
+            </div>
+            <div class = "card-body">
+                <p> ID: ${manager.id}</p>
+                <p> Email: ${manager.email}</p>
+                <p> Office Number: ${manager.office}</p>
+            </div>
+        </div>
+    </div> 
+
+    `;
 }
 
 // engineer card function
@@ -21,5 +36,5 @@ htmlGenerate = (data) => {
 // generates HTML
 
 const generatePage = function (employeeCards) {
-    
+
 }
