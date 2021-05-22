5c7d7a960b6d3a95a848567a06fd42b0a4c641b6
chdonovan <chrisdonovan311@gmail.com>
2021-05-22 17:03:02 -0500

    title html added

--- src/htmlgen.js
+++ src/htmlgen.js
@@ -100,5 +100,20 @@ htmlGenerate = (data) => {
 // generates HTML
 
 const generatePage = function (employeeCards) {
+    return`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name= "viewport" content= "width=device-width, inital -scale=1.0">
+        <title> MY TEAM </title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+    </head>
 
+
+
+`;
 }
+// export to index
+module.exports = htmlGenerate;
